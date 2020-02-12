fbc03a6bbe5757e2aa3ab11c17a04c5904624334
Tim DuBois <tim@neophilus.net>
2020-02-12 13:53:01 +0100

    First attempt at options

--- admin/teambib-admin.php
+++ admin/teambib-admin.php
@@ -3,11 +3,40 @@
 add_action('admin_menu', 'teambib_config');
 
 function teambib_config() {
-    add_menu_page('TeamBIB Configuration', 'TeamBIB', 'manage_options', 'teambib', 'init');
+    add_menu_page('TeamBIB Configuration', 'TeamBIB', 'manage_options', 'teambib', 'admin_page');
 }
 
+add_action('admin_init', 'init');
+
 function init() {
-    echo "<h1>TeamBIB Configuration</h1>";
+    add_settings_section('team-members', 'Team Members', '', 'teambib');
+
+    register_setting('teambib', 'members');
+
+    add_settings_field('members', 'Member List', 'members_cb', 'teambib', 'team-members');
+}
+
+function members_cb() {
+    $members = esc_attr(get_option('members', ''));
+?>
+<div id="titlediv">
+    <input id="title" type="text" name="members" value="<?php echo $members; ?>">
+</div>
+
+<?php
+}
+
+function admin_page() {
+?>
+    <div class="wrap">
+        <?php settings_errors();?>
+        <form method="POST" action="options.php">
+                    <?php settings_fields('teambib');?>
+                    <?php do_settings_sections('teambib')?>
+                    <?php submit_button();?>
+        </form>
+    </div>
+<?php
 }
 
 ?>
