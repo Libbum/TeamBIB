9b4b7afae149fa83c999b56f2eb90b2656475b63
Tim DuBois <tim@neophilus.net>
2020-02-12 14:28:34 +0100

    Attempt to get team-members

--- admin/teambib-admin.php
+++ admin/teambib-admin.php
@@ -3,20 +3,20 @@
 add_action('admin_menu', 'teambib_config');
 
 function teambib_config() {
-    add_menu_page('TeamBIB Configuration', 'TeamBIB', 'manage_options', 'teambib', 'admin_page');
+    add_menu_page('TeamBIB Configuration', 'TeamBIB', 'manage_options', 'teambib', 'teambib_admin_page');
 }
 
-add_action('admin_init', 'init');
+add_action('admin_init', 'teambib_init');
 
-function init() {
+function teambib_init() {
     add_settings_section('team-members', 'Team Members', '', 'teambib');
 
     register_setting('teambib', 'members');
 
-    add_settings_field('members', 'Member List', 'members_cb', 'teambib', 'team-members');
+    add_settings_field('members', 'Member List', 'teambib_members_cb', 'teambib', 'team-members');
 }
 
-function members_cb() {
+function teambib_members_cb() {
     $members = esc_attr(get_option('members', ''));
 ?>
 <div id="titlediv">
@@ -26,7 +26,7 @@ function members_cb() {
 <?php
 }
 
-function admin_page() {
+function teambib_admin_page() {
 ?>
     <div class="wrap">
         <?php settings_errors();?>
--- include/shortcode.php
+++ include/shortcode.php
@@ -3,8 +3,15 @@
 add_shortcode("teambib", "teambib_shortcode");
  
 function teambib_shortcode() {
-    $members = get_option('members', 'Bibliography here.');
-    return $members;
+    global $post;
+
+    $team = array('post_type' => 'tmm', 'name' => 'ses-link-team');
+    $custom_posts = get_posts($team);
+
+    return $custom_posts;
+
+//    $members = get_option('members', 'Bibliography here.');
+//    return $members;
 }
 
 ?>
