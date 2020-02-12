eeb949560785293cce64b82b2c9be69720699694
Tim DuBois <tim@neophilus.net>
2020-02-12 11:01:03 +0100

    Baseline updater requirements

---
+++ TeamBIB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:       TeamBIB
+ * Plugin URI:        https://github.com/Libbum/TeamBIB
+ * Description:       Wordpress / Zotero bridge designed for listing a group's shared publication library.
+ * Version:           1.0.0
+ * Author:            Tim DuBois
+ * License:           Apache v2
+ * License URI:       https://www.apache.org/licenses/LICENSE-2.0
+ * Domain Path:       /languages
+ * Text Domain:       github-updater
+ * GitHub Plugin URI: https://github.com/afragen/github-updater
+ * GitHub Languages:  https://github.com/afragen/github-updater-translations
+ */
+
