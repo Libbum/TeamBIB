7afc2c860ead136c093c61f3f242c30dca148e4a
Tim DuBois <tim@neophilus.net>
2020-02-12 14:34:42 +0100

    Further memberjacking exploration

--- include/shortcode.php
+++ include/shortcode.php
@@ -8,7 +8,25 @@ function teambib_shortcode() {
     $team = array('post_type' => 'tmm', 'name' => 'ses-link-team');
     $custom_posts = get_posts($team);
 
-    return $custom_posts;
+    $bib = '';
+
+    foreach($custom_posts as $post) : setup_postdata($post);
+        $members = get_post_meta( get_the_id(), '_tmm_head', true );
+        if (is_array($members) || is_object($members)) {
+            foreach ($members as $key => $member) {
+                $bib .= '<div class="tmm_names">';
+                if (!empty($member['_tmm_firstname']))
+                    $bib .= '<span class="tmm_fname">'.$member['_tmm_firstname'].'</span> ';
+                if (!empty($member['_tmm_lastname']))
+                    $bib .= '<span class="tmm_lname">'.$member['_tmm_lastname'].'</span>';
+                $bib .= '</div>';
+            }
+        } else {
+            $bib .= 'No member';
+        }
+    endforeach; wp_reset_postdata();
+
+    return $bib;
 
 //    $members = get_option('members', 'Bibliography here.');
 //    return $members;
